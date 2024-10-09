84a9c6f131e6135a5117a89fa955684866256b68
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-09 15:19:33 -0300

    Desafio 2: contagem letra a na string

---
+++ desafio2.py
@@ -0,0 +1,12 @@
+﻿string = input("Digite uma string: ")
+
+contagem_a = 0
+
+for char in string:
+    if char.lower() == 'a':
+        contagem_a += 1
+
+if contagem_a > 0:
+    print(f"A letra 'a' aparece {contagem_a} vezes na string.")
+else:
+    print("A letra 'a' não aparece na string.")
