95b58c076423ce06bf47e1c8b9f34935afadc67d
Gabriel Terto <gabrielterto1002@gmail.com>
2024-10-09 15:17:39 -0300

    Desafio 1: sequência de fibonacci

---
+++ desafio1.py
@@ -0,0 +1,19 @@
+﻿print('-' * 30)
+print('SEQUÊNCIA DE FIBONACCI'.center(30))
+print('-' * 30)
+
+usuario = int(input('Digite um número inteiro: '))
+
+a, b = 0, 1
+encontrado = False
+
+while a <= usuario:
+    if a == usuario:
+        encontrado = True
+        break
+    a, b = b, a + b
+
+if encontrado:
+    print(f'O número {usuario} pertence à sequência de Fibonacci.')
+else:
+    print(f'O número {usuario} não pertence à sequência de Fibonacci.')
